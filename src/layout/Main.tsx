239fffc21dfc6859c0bc3d2f31e0d6c3307f2a44
Arefevpro <arefevmsk@gmail.com>
2024-04-26 12:02:52 +0300

    ./

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,28 +1,31 @@
 import styled from "styled-components";
 import { Container } from "../components/Container";
-import photo from "../img/IMG_9939.jpg"
+import photo from "../img/Remove-bg.ai_1714069446173.png";
+import pattern9 from "../img/3d-grunge-room-interior-with-spotlight-smoky-atmosphere-background.jpg"
+
 
 export const Main = () => {
   return (
-    <Container>
       <Section>
         <ImgWrapper>
-        <Photo src={photo} alt="" />
+          <Photo src={photo} alt="" />
         </ImgWrapper>
       </Section>
-    </Container>
   );
 };
 
 const Section = styled.section`
-  height: 100vh;
+  display: flex;
+  height: 75vh;
+  justify-content: flex-start;
+  align-items: flex-start;
+/* background-image: url(${pattern9}); */
 `;
 
-const ImgWrapper = styled.div `
-  
-`
+const ImgWrapper = styled.div`
 
+`;
 
 const Photo = styled.img`
-  width: 100%;
-`
+  width: 60%;
+`;
